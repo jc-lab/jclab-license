696061fccef01fff4dae3f0200e4b902deb5bfa0
unknown <joseph@jc-lab.net>
2023-11-01 18:12:28 +0900

    feat: msgpack for claim

--- license_model/claim.go
+++ license_model/claim.go
@@ -1,11 +1,13 @@
 package license_model
 
+//go:generate msgp
+
 type Claims struct {
-	Iat               int64  `json:"iat"`
-	Iss               string `json:"iss"`
-	Product           string `json:"product"`
-	LicenseKeyHash    string `json:"license_key_hash"`
-	LicenseMaxVersion int    `json:"license_max_version"`
-	LicenseeName      string `json:"licensee_name"`
-	LicenseeEmail     string `json:"licensee_email"`
+	Iat               int64  `json:"iat" msg:"iat"`
+	Iss               string `json:"iss" msg:"iss"`
+	Product           string `json:"product" msg:"product"`
+	LicenseKeyHash    string `json:"license_key_hash" msg:"license_key_hash"`
+	LicenseMaxVersion int    `json:"license_max_version" msg:"license_max_version"`
+	LicenseeName      string `json:"licensee_name" msg:"licensee_name"`
+	LicenseeEmail     string `json:"licensee_email" msg:"licensee_email"`
 }
